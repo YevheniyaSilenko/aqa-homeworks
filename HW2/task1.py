efe94948df34586463a0aecd0446d3fb10b3dfba
Yevheniia Silenko <prencezza12@gmail.com>
2023-09-03 22:56:20 +0300

    homeworks

---
+++ HW2/task1.py
@@ -0,0 +1,11 @@
+
+
+celsius = float(input("enter temperature in celsius:"))
+
+fahrenheit = celsius * 9/5 + 32
+kelvin = celsius + 273.15
+
+print(f"temperature in: {fahrenheit}°F")
+print(f"temperature in: {kelvin}K")
+
+
