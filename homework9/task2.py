6c58f12e465ddd48b8324f44daa10fe423787d26
Yevheniia Silenko <prencezza12@gmail.com>
2023-10-05 18:13:14 +0300

    task2

--- homework9/task2.py
+++ homework9/task2.py
@@ -1,13 +1,71 @@
 class QAEngineer:
-    def __init__(self, name, years_of_experience, skills):
+    """
+    A class representing a Quality Assurance (QA) Engineer.
+
+    Attributes:
+        name (str): The name of the QA engineer.
+        experience (int): The years of experience in QA testing.
+        skills (list): A list of technical skills possessed by the QA engineer.
+
+    Methods:
+        __init__(self, name, experience, skills): Initializes a new QAEngineer object.
+        get_name(self): Returns the name of the QA engineer.
+        get_experience(self): Returns the years of experience.
+        get_skills(self): Returns the list of technical skills.
+        introduce(self): Prints an introduction message about the QA engineer.
+    """
+
+    def __init__(self, name, experience, skills):
+        """
+        Initializes a new QAEngineer object.
+
+        Args:
+            name (str): The name of the QA engineer.
+            experience (int): The years of experience in QA testing.
+            skills (list): A list of technical skills possessed by the QA engineer.
+        """
         self.name = name
-        self.years_of_experience = years_of_experience
+        self.experience = experience
         self.skills = skills
 
-    def __str__(self):
-        return f"QA Engineer: {self.name}\nYears of Experience: {self.years_of_experience}\nSkills: {', '.join(self.skills)}"
+    def get_name(self):
+        """
+        Returns the name of the QA engineer.
 
-# Usage:
-qa_engineer = QAEngineer("Yevheniia Silenko", 1, ["Manual Testing", "Automated Testing", "Test Case Design"])
-print(qa_engineer)
+        Returns:
+            str: The name of the QA engineer.
+        """
+        return self.name
+
+    def get_experience(self):
+        """
+        Returns the years of experience.
+
+        Returns:
+            int: The years of experience in QA testing.
+        """
+        return self.experience
+
+    def get_skills(self):
+        """
+        Returns the list of technical skills.
+
+        Returns:
+            list: A list of technical skills possessed by the QA engineer.
+        """
+        return self.skills
+
+    def introduce(self):
+        """
+        Prints an introduction message about the QA engineer.
+        """
+        print(f"Hello, I'm {self.name}, a QA Engineer with {self.experience} years of experience.")
+        print("My technical skills include:")
+        for skill in self.skills:
+            print(f"- {skill}")
+
+# Example usage:
+if __name__ == "__main__":
+    qa_engineer = QAEngineer("Yevheniia", 1, ["Manual Testing", "Automation Testing", "Test Planning"])
+    qa_engineer.introduce()
 
