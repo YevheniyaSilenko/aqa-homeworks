1df60bd104b92449a57ddaaa39f32b3c6c6a91b9
Yevheniia Silenko <prencezza12@gmail.com>
2023-10-06 16:20:28 +0300

    task2

--- homework9/task1.py
+++ homework9/task1.py
@@ -1,71 +1,86 @@
-class KoboCompany:
+class Ferrari:
     """
-    A class representing the Kobo company.
-
-    Kobo is a company that specializes in e-readers and e-books.
-
-    Attributes:
-        name (str): The name of the company.
-        headquarters (str): The location of the company's headquarters.
-        founder (str): The name of the company's founder.
-        year_founded (int): The year the company was founded.
+    This class represents the Ferrari company.
     """
 
-    def __init__(self, name, headquarters, founder, year_founded):
-        """
-        Initialize a KoboCompany instance.
+    # Class attribute
+    _total_ferraris = 0  # Hidden attribute
 
-        Args:
-            name (str): The name of the company.
-            headquarters (str): The location of the company's headquarters.
-            founder (str): The name of the company's founder.
-            year_founded (int): The year the company was founded.
+    def __init__(self, model, year, color):
         """
-        self.name = name
-        self.headquarters = headquarters
-        self.founder = founder
-        self.year_founded = year_founded
+        Initialize a Ferrari object.
 
-    def get_name(self):
+        :param model: The model of the Ferrari.
+        :param year: The manufacturing year of the Ferrari.
+        :param color: The color of the Ferrari.
         """
-        Get the name of the company.
+        self._model = model  # Protected attribute
+        self._year = year    # Protected attribute
+        self._color = color  # Protected attribute
+        Ferrari._total_ferraris += 1  # Increment the total Ferrari count
 
-        Returns:
-            str: The name of the company.
+    @property
+    def model(self):
         """
-        return self.name
+        Get the model of the Ferrari.
 
-    def get_headquarters(self):
+        :return: The model of the Ferrari.
         """
-        Get the location of the company's headquarters.
+        return self._model
 
-        Returns:
-            str: The headquarters of the company.
+    @model.setter
+    def model(self, new_model):
         """
-        return self.headquarters
+        Set the model of the Ferrari.
 
-    def get_founder(self):
+        :param new_model: The new model to set.
         """
-        Get the name of the company's founder.
+        self._model = new_model
 
-        Returns:
-            str: The name of the founder.
+    def get_year(self):
         """
-        return self.founder
+        Get the manufacturing year of the Ferrari.
 
-    def get_year_founded(self):
+        :return: The manufacturing year of the Ferrari.
         """
-        Get the year the company was founded.
+        return self._year
 
-        Returns:
-            int: The year the company was founded.
+    def set_year(self, new_year):
         """
-        return self.year_founded
+        Set the manufacturing year of the Ferrari.
+
+        :param new_year: The new manufacturing year to set.
+        """
+        self._year = new_year
+
+    @staticmethod
+    def get_total_ferraris():
+        """
+        Get the total number of Ferrari objects created.
+
+        :return: The total number of Ferrari objects.
+        """
+        return Ferrari._total_ferraris
+
+    def __str__(self):
+        """
+        Return a string representation of the Ferrari.
+
+        :return: A string describing the Ferrari.
+        """
+        return f"A {self._color} Ferrari {self._model} manufactured in {self._year}"
 
 # Example usage:
 if __name__ == "__main__":
-    kobo = KoboCompany("Kobo Inc.", "Toronto, Canada", "Michael Serbinis", 2009)
-    print("Company Name:", kobo.get_name())
-    print("Headquarters:", kobo.get_headquarters())
-    print("Founder:", kobo.get_founder())
-    print("Year Founded:", kobo.get_year_founded())
+    ferrari1 = Ferrari("488 GTB", 2023, "Red")
+    ferrari2 = Ferrari("F8 Tributo", 2022, "Yellow")
+
+    print(ferrari1)
+    print(ferrari2)
+
+    ferrari1.model = "812 Superfast"
+    ferrari1.set_year(2024)
+
+    print(ferrari1)
+
+    print(f"Total Ferrari cars created: {Ferrari.get_total_ferraris()}")
